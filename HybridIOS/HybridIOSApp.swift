e7de64093b2906fdb7461a595726078c822c0235
Amadou TIRERA <amadoutirera@msn.com>
2023-08-16 13:49:47 +0200

    Initial Commit

---
+++ HybridIOS/HybridIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridIOSApp.swift
+//  HybridIOS
+//
+//  Created by Amadou TIRERA on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HybridIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
